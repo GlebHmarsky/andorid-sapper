6f760e09736972afb5d4ab5354cb2936d6eeab01
Gleb Hmarsky <hmarsky.gleb@yandex.ru>
2022-04-03 00:07:52 +0300

    added path from victory screen to main

--- app/src/main/java/com/example/sapper/VictoryFragment.kt
+++ app/src/main/java/com/example/sapper/VictoryFragment.kt
@@ -1,19 +1,35 @@
 package com.example.sapper
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
+import com.example.sapper.databinding.FragmentVictoryBinding
 
 
 class VictoryFragment : Fragment() {
-   override fun onCreateView(
+  private lateinit var binding: FragmentVictoryBinding
+  override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_victory, container, false)
+
+    binding = DataBindingUtil.inflate(inflater, R.layout.fragment_victory, container, false)
+    val gameContentFragmentArgs by navArgs<LoseFragmentArgs>()
+
+    binding.timeText.text =
+      DateUtils.formatElapsedTime(gameContentFragmentArgs.time.toLong()).toString()
+
+    binding.buttonMain.setOnClickListener {
+      Navigation.findNavController(it).navigate(R.id.action_loseFragment_to_titleFragment)
+    }
+    return binding.root
   }
+
 
 }
--- app/src/main/java/com/example/sapper/enums/LevelDifficulty.kt
+++ app/src/main/java/com/example/sapper/enums/LevelDifficulty.kt
@@ -1,7 +1,7 @@
 package com.example.sapper.enums
 
 enum class LevelDifficulty(val size: Int, val bombs: Int) {
-  EASY(5, 1),
+  EASY(5, 7),
   MEDIUM(8, 13),
   HARD(10, 25)
 }
